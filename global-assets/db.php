aeae18079be4599e84d0dcfe76e07f2471d1f68e
Rivindu2003 <it23552210@my.sliit.lk>
2024-10-05 02:04:49 +0530

    password-logins

--- global-assets/db.php
+++ global-assets/db.php
@@ -2,9 +2,9 @@
 
 // Database credentials
 $host = 'localhost'; // Usually 'localhost'
-$username = 'rivindu'; // Your database username
-$password = 'Rivindu@1234'; // Your database password
-$database = 'shop_management'; // Your database name
+$username = 'bubb_rivindu'; // Your database username
+$password = 'fJNwC%AtfDA!2mPe'; // Your database password
+$database = 'bubb_bubbleshine'; // Your database name
 
 // Create a connection
 $connection = mysqli_connect($host, $username, $password, $database);
